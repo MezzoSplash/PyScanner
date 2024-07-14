4adf96ff323101c6277451a039b0093c97617a12
MezzoSplash <mezzosplash@gmail.com>
2024-07-14 23:47:51 +0200

    sex

--- ScannerGG.py
+++ ScannerGG.py
@@ -8,8 +8,9 @@ import sys
 def onePing(ipinput):
     #check if input is a string first
     if type(ipinput) != str:
-        return "not correct, try again nerd"
-    # Make the command, split input
+        return "not correct, try again"
+
+    #Make the command, split input
     doPing = "ping -c 1 {}".format(ipinput).split()
     #print result and hide output of subprocess
     result = subprocess.run(doPing, stdout=subprocess.DEVNULL)
